5bc3af95babb8bbe060f09170f2d6c31faffd1b1
wakaba0972 <benny.wu2199@gmail.com>
2024-03-24 02:04:13 +0800

    fix

--- APCS/201603/p3.cpp
+++ APCS/201603/p3.cpp
@@ -1,66 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define MAX 100001
-typedef unsigned long long ull;
-
-int n;
-int tree[MAX << 3];
-
-void update(int v, int g, int l, int r, int pos=0){
-    if(l == r && l == g){
-        tree[pos] += v;
-        return;
-    }
-
-    int mid = (l + r) >> 1;
-
-    if(g <= mid) update(v, g, l, mid, (pos << 1)+1);
-    else update(v, g, mid+1, r, (pos << 1)+2);
-
-    tree[pos] =  tree[(pos << 1)+1] + tree[(pos << 1)+2];
-    return;
-}
-
-ull query(int gl, int gr, int l, int r, int pos=0){
-    if(l == gl && r == gr){
-        return tree[pos];
-    }
-
-    int mid = (l + r) >> 1;
-
-    if(gr <= mid){
-        return query(gl, gr, l, mid, (pos << 1)+1);
-    }
-    else if(gl > mid){
-        return query(gl, gr, mid+1, r, (pos << 1)+2);
-    }
-    else{
-        return query(gl, mid, l, mid, (pos << 1)+1) + query(mid+1, gr, mid+1, r, (pos << 1)+2);
-    }
-}
-
 int main(){
-    cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+    cin.tie(), cout.tie(), ios::sync_with_stdio(0);
 
-    ull ans = 0;
-    map<int, priority_queue<int>> mp;
-
+    int n;
     cin >> n;
-    for(int t, i=0; i<(n << 1); i++){
-        cin >> t;
-        mp[t].push(i);
+
+    priority_queue<pair<int, int>> pq;
+    for(int l, r, i=0; i<n; i++){
+        cin >> l >> r;
+        pq.push({-l, r});
     }
 
-    for(auto it=mp.begin(); it!=mp.end(); it++){
-        int r = it->second.top();
-        it->second.pop();   
-        int l = it->second.top();
-        it->second.pop();
+    int ans = 0;
+    while(!pq.empty()){
+        auto cur = pq.top();
+        pq.pop();
 
-        ans += query(l, r, 0, (n << 1) - 1);
-        update(1, l, 0, (n << 1) - 1);
-        update(1, r, 0, (n << 1) - 1);
+        if(cur.second < -pq.top().first) ans += (cur.second + cur.first);
+        else {
+            while(!pq.empty() && cur.second >= -pq.top().first) {
+                cur.second = max(cur.second, pq.top().second);
+                pq.pop();
+            }
+            ans += (cur.second + cur.first);
+        }
     }
 
     cout << ans;
