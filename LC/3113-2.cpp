17816d25df266024f339e4a64206198102f1e606
wakaba0972 <benny.wu2199@gmail.com>
2024-04-14 02:58:07 +0800

    2024/4/13

---
+++ LC/3113-2.cpp
@@ -0,0 +1,39 @@
+/*
+    單調遞減隊列，
+    如果x > top，清空stack
+    如果stack為空，將x加入
+    如果top == x，ans += cnt(x), cnt(x)++，(只有x連續時cnt會增加)
+    如果top > x，將x加入，ans += 1
+*/
+
+class Solution {
+public:
+    long long numberOfSubarrays(vector<int>& nums) {
+        long long ans = 0;
+        stack<vector<int>> stk;
+
+        for(int i: nums){
+            while(!stk.empty() && stk.top()[0] < i) stk.pop();
+
+            if(stk.empty()) {
+                stk.push({i, 1});
+                ans += 1;
+                continue;
+            }
+
+            if(stk.top()[0] == i){
+                stk.top()[1]++;
+                ans += stk.top()[1];
+                continue;
+            }
+
+            if(stk.top()[0] > i) {
+                stk.push({i, 1});
+                ans += 1;
+                continue;    
+            }
+        }
+
+        return ans;
+    }
+};
