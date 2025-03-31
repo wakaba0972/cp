582e4cee1aff54688a51e38c62bc097676d24dd7
wakaba0972 <benny.wu2199@gmail.com>
2025-03-31 14:23:07 +0800

    Frog_1 remake

--- AtCoder/DP Contest/Frog_1.cpp
+++ AtCoder/DP Contest/Frog_1.cpp
@@ -1,26 +1,29 @@
+/*
+	設f(i)為到達第i個石頭的最小花費，
+	則有初始狀態 f(0) = 0, f(1) = abs(stones[1]-stones[0])
+	轉移方程 f(i) = min(f(i-1)+abs(stones[i]-stones[i-1]), f(i-2)+abs(stones[i]-stones[i-2]))
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
-#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0)
-#define ll long long
-#define ull unsigned long long
-
-int N;
-vector<int> vs(100001);
-vector<int> dp(100001);
-
 int main(){
-	fastio;
+	// input
+	int n;
+	cin >> n;
 
-	cin >> N;
-	for(int i=0; i<N; i++) cin >> vs[i];
-
-	dp[1] = abs(vs[1] - vs[0]);
-    
-    for(int i=2; i<N; i++){
-    	dp[i] = min(abs(vs[i] - vs[i-1]) + dp[i-1], abs(vs[i] - vs[i-2]) + dp[i-2]);
-    }
-    
-    cout << dp[N-1];
-    
+	vector<int> stones(n);
+	for(int i=0; i<n; ++i) cin >> stones[i];
+	
+	// init
+	vector<int> dp(n);
+	dp[1] = abs(stones[1]-stones[0]);
+	
+	// trans
+	for(int i=2; i<n; ++i){
+		dp[i] = min(abs(stones[i]-stones[i-1])+dp[i-1], abs(stones[i]-stones[i-2])+dp[i-2]);
+	}
+	
+	// output
+	cout << dp[n-1];
 }
